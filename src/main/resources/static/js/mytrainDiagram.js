71453b24ced16ba4ff0a5cf01300bb260bca2c17
Alabbas Alhaj Ali <weeamha@gmail.com>
2018-12-18 11:41:07 +0100

    :sparkles: #update the render of nodes and links, fix the tooltip

--- src/main/resources/static/js/mytrainDiagram.js
+++ src/main/resources/static/js/mytrainDiagram.js
@@ -2,68 +2,87 @@
 
 let mapSvgContainer, tip
 
-/*  Node Positions
+/*  Node Positions Converters
 *************************************************************/
 const spider = {
-  "place-asmnl": [7.071067812, 14.82842712],
-  "place-smmnl": [7.071067812, 13.82842712],
-  "place-fldcr": [7.071067812, 12.82842712],
-  "place-shmnl": [7.071067812, 11.82842712],
-  "place-jfk": [7.778174593, 11.12132034],
-  "place-andrw": [7.778174593, 10.12132034],
-  "place-brdwy": [7.778174593, 9.121320344],
-  "place-sstat": [7.778174593, 8.121320344],
-  "place-dwnxg": [7.071067812, 7.414213562],
-  "place-pktrm": [6.363961031, 6.707106781],
-  "place-chmnl": [5.656854249, 6],
-  "place-knncl": [4.949747468, 5.292893219],
-  "place-cntsq": [4.242640687, 4.585786438],
-  "place-harsq": [3.535533906, 3.878679656],
-  "place-portr": [2.828427125, 3.171572875],
-  "place-davis": [1.828427125, 3.171572875],
-  "place-alfcl": [0.8284271247, 3.171572875],
-  "place-nqncy": [8.485281374, 11.82842712],
-  "place-wlsta": [9.192388155, 12.53553391],
-  "place-qnctr": [9.899494937, 13.24264069],
-  "place-qamnl": [10.60660172, 13.94974747],
-  "place-brntn": [10.60660172, 14.94974747],
-  "place-wondl": [14.43502884, 2.464466094],
-  "place-rbmnl": [13.72792206, 3.171572875],
-  "place-bmmnl": [13.02081528, 3.878679656],
-  "place-sdmnl": [12.3137085, 4.585786438],
-  "place-orhte": [11.60660172, 5.292893219],
-  "place-wimnl": [10.89949494, 6],
-  "place-aport": [10.19238816, 6.707106781],
-  "place-mvbcl": [9.485281374, 7.414213562],
-  "place-aqucl": [8.485281374, 7.414213562],
-  "place-gover": [7.071067812, 6],
-  "place-bomnl": [6.363961031, 5.292893219],
-  "place-forhl": [0, 14.48528137],
-  "place-grnst": [0.7071067812, 13.77817459],
-  "place-sbmnl": [1.414213562, 13.07106781],
-  "place-jaksn": [2.121320344, 12.36396103],
-  "place-rcmnl": [2.828427125, 11.65685425],
-  "place-rugg": [3.535533906, 10.94974747],
-  "place-masta": [4.242640687, 10.24264069],
-  "place-bbsta": [4.949747468, 9.535533906],
-  "place-tumnl": [5.656854249, 8.828427125],
-  "place-chncl": [6.363961031, 8.121320344],
-  "place-state": [7.778174593, 6.707106781],
-  "place-haecl": [8.485281374, 6],
-  "place-north": [8.485281374, 5],
-  "place-ccmnl": [8.485281374, 4],
-  "place-sull": [8.485281374, 3],
-  "place-welln": [8.485281374, 2],
-  "place-mlmnl": [8.485281374, 1],
-  "place-ogmnl": [8.485281374, 0]
+  "1": [7.071067812, 14.82842712],
+  "2": [7.071067812, 13.82842712],
+  "3": [7.071067812, 12.82842712],
+  "4": [7.071067812, 11.82842712],
+  "5": [7.778174593, 11.12132034],
+  "6": [7.778174593, 10.12132034],
+  "7": [7.778174593, 9.121320344],
+  "8": [7.778174593, 8.121320344],
+  "9": [7.071067812, 7.414213562],
+  "10": [6.363961031, 6.707106781],
+  "11": [5.656854249, 6],
+  "12": [4.949747468, 5.292893219],
+  "13": [4.242640687, 4.585786438],
+  "14": [3.535533906, 3.878679656],
+  "15": [2.828427125, 3.171572875],
+  "16": [1.828427125, 3.171572875],
+  "17": [0.8284271247, 3.171572875],
+  "18": [8.485281374, 11.82842712],
+  "19": [9.192388155, 12.53553391],
+  "20": [9.899494937, 13.24264069],
+  "21": [10.60660172, 13.94974747],
+  "22": [10.60660172, 14.94974747],
+  "23": [14.43502884, 2.464466094],
+  "24": [13.72792206, 3.171572875],
+  "25": [13.02081528, 3.878679656],
+  "26": [12.3137085, 4.585786438],
+  "27": [11.60660172, 5.292893219],
+  "28": [10.89949494, 6],
+  "29": [10.19238816, 6.707106781],
+  "30": [9.485281374, 7.414213562],
+  "31": [8.485281374, 7.414213562],
+  "32": [7.071067812, 6],
+  "33": [6.363961031, 5.292893219],
+  "34": [0, 14.48528137],
+  "35": [0.7071067812, 13.77817459],
+  "36": [1.414213562, 13.07106781],
+  "37": [2.121320344, 12.36396103],
+  "38": [2.828427125, 11.65685425],
+  "39": [3.535533906, 10.94974747],
+  "40": [4.242640687, 10.24264069],
+  "41": [4.949747468, 9.535533906],
+  "42": [5.656854249, 8.828427125],
+  "43": [6.363961031, 8.121320344],
+  "44": [7.778174593, 6.707106781],
+  "45": [8.485281374, 6],
+  "46": [8.485281374, 5],
+  "47": [8.485281374, 4],
+  "48": [8.485281374, 3],
+  "49": [8.485281374, 2],
+  "50": [8.485281374, 1],
+  "51": [8.485281374, 0]
 }
 
-const idToNodeFn = (nodes) => {
-    return nodes.map( node => {
+const convertNodesIdToPostion = (network) => {
+    return network.nodes.map(node => {
         node.x = spider[node.id][0]
         node.y = spider[node.id][1]
         return node
     })
+}
+
+const convertLinksToPostions = (network) => {
+    const links = network.links.filter( link => {
+        const sourceNode = network.nodes.find(it =>  it.id === link.source.toString())
+        const targetNode = network.nodes.find(it => it.id === link.target.toString())
+        return sourceNode && targetNode
+    }).map(link => {
+        const sourceNode = network.nodes.find(it =>  it.id === link.source.toString())
+        const targetNode = network.nodes.find(it => it.id === link.target.toString())
+        link.source = {'nodeID': link.source }
+        link.source.x = sourceNode.x
+        link.source.y = sourceNode.y
+        link.target = { 'nodeID': link.target }
+        link.target.x = targetNode.x
+        link.target.y = targetNode.y
+        return link
+    })
+    return links
 }
 
 /*  Constants Decleration Section
@@ -111,13 +130,8 @@ const createMap = (parentTagID) => {
         .attr("height", height)
         .append('g', 'map-container')
         .attr("transform", "translate(" + MARGIN.LEFT + "," + MARGIN.TOP + ")")
-    tip = d3.tip()
-        .attr('class', 'd3-tip')
-        .html(d => d.name)
-    svg.call(tip)
     return svg
 }
-
 
 /*  Create NetWork on the Map
 *************************************************************/
@@ -128,41 +142,38 @@ const renderMap = (mapSvgContainer, network) => {
     const connections = mapSvgContainer.selectAll('.connect')
         .data(network.links, d => (d.source && d.source.id) + '-' + (d.target && d.target.id))
     
-    connections
-        .enter()
-      .append('line')
+    connections.enter()
+        .append('line')
         .attr('class', d => 'connect ' + d.line + '-dimmable')
-        .attr('x1', d => d.sourc)
-        .attr('y1', d => d.sourc)
-        .attr('x2', d => d.target)
-        .attr('y2', d => d.target)
-
-    connections
-        .attr('x1', d => d.sourc)
-        .attr('y1', d => d.sourc)
-        .attr('x2', d => d.target)
-        .attr('y2', d => d.target)
+        .attr('x1', d => d.source.x * 30)
+        .attr('y1', d => d.source.y * 30)
+        .attr('x2', d => d.target.x * 30)
+        .attr('y2', d => d.target.y * 30)
     
-    stations
-        .enter()
-      .append('circle')
-        .attr('class', d => 'station middle station-label ' + d.id)
-        .on('mouseover', d => {
-            if (d < 30) {
-                tip.direction('e').offset([0, 10])
-            } else {
-                tip.direction('n').offset([-10, 0])
-            }
-            tip.show(d);
-            highlightMareyTitle(d.id, _.unique(d.links.map(link => { return link.line })))
-        })
-        .on('mouseout', d => {
-            tip.hide(d) 
-            highlightMareyTitle(null)
-        })
+    stations.enter()
+        .append('circle')
+        .attr('class', d => 'station middle station-label ' + d.id + ' ' + d.type)
         .attr('cx', d =>  d.x * 30)
         .attr('cy', d =>  d.y * 30)
         .attr('r', 3)
+        .style("fill", d => {
+            d.color = colorScale(d.type.replace(/ .*/, ""))
+            return d.color
+        })
+        .style("stroke", d => d3.rgb(colorScale(d.type.replace(/ .*/, ""))).darker(0.1))
+        .on('mouseover', d => {
+            const xPosition = parseFloat(d.x * 30)
+			const yPosition = parseFloat(d.y * 30 + 50)
+            d3.select("#tooltip")
+				.style("left", xPosition + "px")
+				.style("top", yPosition + "px")
+				.select("#value")
+                .text(d.name)
+            d3.select("#tooltip").classed("hidden", false)
+        })
+        .on('mouseout', _d => {
+            d3.select("#tooltip").classed("hidden", true)
+        })
 
     stations
         .attr('cx', d => d.x * 30)
@@ -178,10 +189,10 @@ const update = (network) => {
     if (!mapSvgContainer)
         mapSvgContainer = createMap(PARENT_TAG_ID)
     
-    network.nodes = idToNodeFn(network.nodes)
+    network.nodes = convertNodesIdToPostion(network)
+    network.links = convertLinksToPostions(network)
 
     renderMap(mapSvgContainer, network)
-    
 }
 
 /* 1. Load and pre-process the data
