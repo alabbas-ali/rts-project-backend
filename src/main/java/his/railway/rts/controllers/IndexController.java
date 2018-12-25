4a78955500495ac4039f7f5ed0f9bf75db895a75
Alabbas Alhaj Ali <weeamha@gmail.com>
2018-12-25 18:37:03 +0100

    :sparkles: add get method for Json Railway

--- src/main/java/his/railway/rts/controllers/IndexController.java
+++ src/main/java/his/railway/rts/controllers/IndexController.java
@@ -4,6 +4,12 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import his.railway.rts.model.HttpResponceStatus;
+import his.railway.rts.model.JsonResponseBody;
+import his.railway.rts.reader.RailwayReader;
 
 @Controller
 public class IndexController {
@@ -13,5 +19,22 @@ public class IndexController {
 		return "index";
 	}
 	
+	@ResponseBody
+	@RequestMapping(value = "/get", method= RequestMethod.GET)
+	public JsonResponseBody getRailway() {
+		
+		JsonResponseBody response = new JsonResponseBody();
+		
+		RailwayReader reader= new RailwayReader();
+		ClassLoader classLoader = getClass().getClassLoader();
+		try {
+			response.setResult(reader.read(classLoader.getResource("input/railway.json")));
+			response.setStatus(HttpResponceStatus.SUCCESS);
+		} catch (Exception e) {
+			response.setResult(e.getMessage());
+			response.setStatus(HttpResponceStatus.FAIL);
+			e.printStackTrace();
+		}
+		return response;
+	}
 }
-
