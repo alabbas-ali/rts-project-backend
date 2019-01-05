f0aced4ae04f361863722cc81f42bc49e31748ea
Alabbas Alhaj Ali <weeamha@gmail.com>
2019-01-05 16:23:18 +0100

    :+1: clean Index Controller

--- src/main/java/his/railway/rts/controllers/IndexController.java
+++ src/main/java/his/railway/rts/controllers/IndexController.java
@@ -4,13 +4,11 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import his.railway.rts.job.JobRunner;
 import his.railway.rts.model.HttpResponceStatus;
 import his.railway.rts.model.JsonResponseBody;
 import his.railway.rts.service.RailwayService;
@@ -20,12 +18,9 @@ public class IndexController {
 
 	@Autowired
 	private RailwayService railwayService;
-
-	@Autowired
-	private SimpMessagingTemplate template;
 	
-	//@Autowired
-	//private ArduinoReadeWriteService arduino;
+//	@Autowired
+//	private ArduinoReadeWriteService arduino;
 
 	@RequestMapping("/")
 	public String index(Map<String, Object> model) {
@@ -36,9 +31,6 @@ public class IndexController {
 	@ResponseBody
 	@SendTo("/railway/status")
 	public JsonResponseBody fetchStatus() {
-		JobRunner job = new JobRunner(railwayService, template);
-		Thread thread = new Thread(job);
-		thread.start();
 		JsonResponseBody response = new JsonResponseBody();
 		response.setResult(railwayService.getRailway());
 		response.setStatus(HttpResponceStatus.SUCCESS);
