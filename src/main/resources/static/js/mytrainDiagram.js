1bf4de993891db591377719dd7b31675bc7faa2d
Alabbas Alhaj Ali <weeamha@gmail.com>
2019-02-03 12:11:52 +0100

    :sparkles: implements grapg update according to backend masseges

--- src/main/resources/static/js/mytrainDiagram.js
+++ src/main/resources/static/js/mytrainDiagram.js
@@ -1,47 +1,9 @@
 'use strict'
 
-let mapSvgContainer, tip
-
-/*  Node Positions Converters
- *************************************************************/
-let spider = {}
-
-const convertNodesIdToPostion = (network) => {
-    return network.nodes.map(node => {
-        node.x = spider[node.id][0]
-        node.y = spider[node.id][1]
-        return node
-    })
-}
-
-const convertLinksToPostions = (network) => {
-    const links = network.links.filter(link => {
-        const sourceNode = network.nodes.find(it => it.id === link.source.toString())
-        const targetNode = network.nodes.find(it => it.id === link.target.toString())
-        return sourceNode && targetNode
-    }).map(link => {
-        const sourceNode = network.nodes.find(it => it.id === link.source.toString())
-        const targetNode = network.nodes.find(it => it.id === link.target.toString())
-        link.source = {
-            'nodeID': link.source,
-            'type' : sourceNode.type
-        }
-        link.source.x = sourceNode.x
-        link.source.y = sourceNode.y
-        link.target = {
-            'nodeID': link.target,
-            'type' : targetNode.type
-        }
-        link.target.x = targetNode.x
-        link.target.y = targetNode.y
-        return link
-    })
-    return links
-}
+let mapSvgContainer, tip, network
 
 /*  Constants Decleration Section
  *************************************************************/
-
 const PARENT_TAG_ID = "#chart",
     ZUME = 30,
     OPACITY = {
@@ -56,13 +18,7 @@ const PARENT_TAG_ID = "#chart",
     TYPES_RADIUS = {
         'GATE': 3,
         'CONNECTION_GATE': 6,
-        'STASTION': 2
     },
-    TYPE_COLORS = ["#1b9e77", "#d95f02", "#7570b3"],
-    TYPE_HIGHLIGHT_COLORS = ["#66c2a5", "#fc8d62", "#8da0cb"],
-    LINK_COLOR = "#b3b3b3",
-    INFLOW_COLOR = "#2E86D1",
-    OUTFLOW_COLOR = "#D63028",
     NODE_WIDTH = 36,
     COLLAPSER = {
         RADIUS: NODE_WIDTH / 2,
@@ -76,10 +32,56 @@ const PARENT_TAG_ID = "#chart",
         LEFT: OUTER_MARGIN
     },
     LAYOUT_INTERATIONS = 32,
-    REFRESH_INTERVAL = 7000;
+    REFRESH_INTERVAL = 7000,
+    STASTION_STATUS = {
+        GREEN: 'GREEN',
+        ORANGE: 'ORANGE',
+        RED: 'RED',
+    },
+    LINK_STATUS = {
+        GREEN: 'GREEN',
+        RED: 'RED',
+    }
 
-const colorScale = d3.scaleOrdinal().domain(TYPES).range(TYPE_COLORS)
-const highlightColorScale = d3.scaleOrdinal().domain(TYPES).range(TYPE_HIGHLIGHT_COLORS)
+/*  Node Positions Converters
+ *************************************************************/
+let spider = {}
+
+const convertNodesIdToPostion = (network) => {
+    return network.nodes.map(node => {
+        node.x = spider[node.id][0]
+        node.y = spider[node.id][1]
+        node.status = STASTION_STATUS.GREEN
+        return node
+    })
+}
+
+const convertLinksToPostions = (network) => {
+    const links = network.links.filter(link => {
+        const sourceNode = network.nodes.find(it => it.id === link.source.toString())
+        const targetNode = network.nodes.find(it => it.id === link.target.toString())
+        return sourceNode && targetNode
+    }).map(link => {
+        const sourceNode = network.nodes.find(it => it.id === link.source.toString())
+        const targetNode = network.nodes.find(it => it.id === link.target.toString())
+        link.source = {
+            nodeID: parseInt(link.source, 10),
+            type: sourceNode.type
+        }
+        link.x1 = sourceNode.x
+        const num1 = sourceNode.type === 'STASTION' && link.direction === 2 ? 0.5 : 0
+        link.y1 = sourceNode.y + num1
+        link.target = {
+            nodeID: parseInt(link.target, 10),
+            type: targetNode.type
+        }
+        link.x2 = targetNode.x
+        const num2 = targetNode.type === 'STASTION' && link.direction === 2 ? 0.5 : 0
+        link.y2 = targetNode.y + num2
+        return link
+    })
+    return links
+}
 
 /*  Create The Map Continer
  *************************************************************/
@@ -109,24 +111,10 @@ const renderMap = (mapSvgContainer, network) => {
         .attr('class', d => 'connect ' + d.status + '-dimmable')
         .attr('source', d => d.source.nodeID)
         .attr('target', d => d.target.nodeID)
-        .attr('x1', d => d.source.x * ZUME)
-        .attr('y1', d => {
-            const num = d.source.type === 'STASTION' && d.direction === 2 ? 0.5 : 0
-            return (d.source.y + num) * ZUME
-        })
-        .attr('x2', d => d.target.x * ZUME)
-        .attr('y2', d => {
-            const num = d.target.type === 'STASTION' && d.direction === 2 ? 0.5 : 0
-            return (d.target.y + num) * ZUME
-        })
-
-    // connections.enter()
-    //     .append('text')
-    //     .attr('x', d => (d.source.x * ZUME + d.target.x * ZUME)/2)
-    //     .attr('y', d => (d.source.y * ZUME + d.target.y * ZUME)/2)
-    //     .attr('text-anchor', 'middle')
-    //     .attr('class', 'connect label')
-    //     .text(d=> d.source.nodeID + '-' + d.target.nodeID)
+        .attr('x1', d => d.x1 * ZUME)
+        .attr('y1', d => d.y1 * ZUME)
+        .attr('x2', d => d.x2 * ZUME)
+        .attr('y2', d => d.y2 * ZUME)
 
     nodes.enter()
         .append('text')
@@ -139,49 +127,19 @@ const renderMap = (mapSvgContainer, network) => {
     network.nodes.forEach(node => {
         if (node.type == 'STASTION')
             mapSvgContainer.append('rect')
-            .attr('class',' middle station-label ' + node.id + ' ' + node.type)
+            .attr('class', ' middle ' + node.type + '-node ' + node.status)
             .attr('id', node.id)
             .attr('x', node.x * ZUME)
             .attr('y', (node.y - 0.2) * ZUME)
             .attr("width", 15)
             .attr("height", 28)
-            .style("fill", colorScale(node.type.replace(/ .*/, "")))
-            .style("stroke", d3.rgb(colorScale(node.type.replace(/ .*/, ""))).darker(0.1))
-            .on('mouseover', d => {
-                const xPosition = parseFloat(node.x * ZUME)
-                const yPosition = parseFloat(node.y * ZUME + 80)
-                d3.select("#tooltip")
-                    .style("left", xPosition + "px")
-                    .style("top", yPosition + "px")
-                    .select("#value")
-                    .text(node.name + ' ' + node.id)
-                d3.select("#tooltip").classed("hidden", false)
-            })
-            .on('mouseout', _d => {
-                d3.select("#tooltip").classed("hidden", true)
-            })
         else
             mapSvgContainer.append('circle')
-            .attr('class', ' middle station-label ' + node.id + ' ' + node.type)
+            .attr('class', ' middle ' + node.type + '-node ' + node.status)
             .attr('id', node.id)
             .attr('cx', node.x * ZUME)
             .attr('cy', node.y * ZUME)
             .attr('r', TYPES_RADIUS[node.type])
-            .style("fill", colorScale(node.type.replace(/ .*/, "")))
-            .style("stroke", d3.rgb(colorScale(node.type.replace(/ .*/, ""))).darker(0.1))
-            .on('mouseover', d => {
-                const xPosition = parseFloat(node.x * ZUME)
-                const yPosition = parseFloat(node.y * ZUME + 80)
-                d3.select("#tooltip")
-                    .style("left", xPosition + "px")
-                    .style("top", yPosition + "px")
-                    .select("#value")
-                    .text(node.name + ' ' + node.id)
-                d3.select("#tooltip").classed("hidden", false)
-            })
-            .on('mouseout', _d => {
-                d3.select("#tooltip").classed("hidden", true)
-            })
     });
 
 }
@@ -189,32 +147,175 @@ const renderMap = (mapSvgContainer, network) => {
 /* Update Function When there are new Data
  *************************************************************/
 const update = (message) => {
-    console.log(message)
-    if (message.id)
-        updateCircle(node)
-    else(message.target)
-    updateLine(node)
+    if (!network)
+        return null
+    
+    if (!network.labels) 
+        network.labels = []
+
+    switch (message.command) {
+        case "InterStation":
+            interStation(message)
+            break;
+        case "InterLine":
+            interLine(message)
+            break;
+        case "ChangeSwitch":
+            changeSwitch(message)
+            break;
+    }
+
 }
 
-const updateCircle = (node) => {
-    //console.log("Change Circle {" + node.id + "," + node.type  + "," + node.name + "," + "}")
-    mapSvgContainer.select("circle[id='" + node.id + "']")
-        .style("fill", d => {
-            d.color = colorScale(node.type.replace(/ .*/, ""))
-            return d.color
-        })
-        .style("stroke", _d => d3.rgb(colorScale(node.type.replace(/ .*/, ""))).darker(0.1))
+const interStation = (message) => {
+    // console.log(message)
+    var previousLink = network.links.find(
+        el => el.target.nodeID === message.station &&
+        el.direction === message.direction
+    )
+    // console.log(previousLink)
+    var node = network.nodes.find(
+        nd => parseInt(nd.id, 10) === message.station
+    )
+    if (node.status === STASTION_STATUS.RED) {
+        console.error("Cannt Enter a full Station", message)
+        //return
+    }
+    previousLink.status = LINK_STATUS.GREEN
+    node.status = node.status === STASTION_STATUS.GREEN ?
+        STASTION_STATUS.ORANGE :
+        STASTION_STATUS.RED
+    updateLine(previousLink)
+
+    let label = network.labels.find(la => la.id === message.train)
+    const x = (node.x + 0.1) * ZUME
+    const y = message.direction = 1 ? node.y * ZUME : (node.y + 0.6) * ZUME
+    if (!label) {
+        label = { 
+            id: message.train, 
+            x, 
+            y,
+        }
+        network.labels.push(label)
+        appendLabel(label)
+    } else {
+        label.x = x
+        label.y = y
+        updateLabel(label)
+    }
+
+    updateStation(node)
 }
 
-const updateLine = (node) => {
-    //console.log("Change Line {" + node.source + "," + node.target  + "," + node.status + "," + "}")
-    mapSvgContainer.select("line[source='" + node.source + "'][target='" + node.target + "']")
-        .attr('class', 'connect ' + node.status + '-dimmable')
+const interLine = (message) => {
+    let link = network.links.find(
+        el => el.source.nodeID === message.from &&
+        el.target.nodeID === message.to &&
+        el.direction === message.direction
+    )
+    if (link.status === LINK_STATUS.RED) {
+        console.error("Cannt Enter a full Line", message)
+        //return
+    }
+    if (link.source.type === 'STASTION') {
+        let previousNode = network.nodes.find(
+            nd => parseInt(nd.id, 10) === message.from
+        )
+        previousNode.status = previousNode.status === STASTION_STATUS.ORANGE ?
+            STASTION_STATUS.GREEN :
+            STASTION_STATUS.ORANGE
+        updateStation(previousNode)
+    } else if (link.source.type === 'GATE') {
+        let previousLink = network.links.find(
+            el => el.target.nodeID === message.from &&
+            el.direction === message.direction &&
+            el.status === LINK_STATUS.RED
+        )
+        previousLink.status = LINK_STATUS.GREEN
+        updateLine(previousLink)
+    }else if(link.source.type === 'CONNECTION_GATE'){
+        let previousLink = network.links.find(
+            el => el.target.nodeID === message.from
+        )
+        previousLink.status = LINK_STATUS.GREEN
+        updateLine(previousLink)
+    }
+
+    let label = network.labels.find(la => la.id === message.train)
+    const x = (link.x1 * ZUME + link.x2 * ZUME)/2
+    const y = message.direction = 1 ?  
+        (link.y1 * ZUME + link.y2 * ZUME)/2 :
+        ((link.y1 + 0.5) * ZUME + (link.y2 + 0.5) * ZUME)/2
+    if (!label) {
+        label = { 
+            id: message.train, 
+            x, 
+            y,
+        }
+        network.labels.push(label)
+        appendLabel(label)
+    } else {
+        label.x = x
+        label.y = y
+        updateLabel(label)
+    }
+    
+    link.status = LINK_STATUS.RED
+    updateLine(link)
+}
+
+const changeSwitch = (message) => {
+    let Switch = network.nodes.find(
+        nd => parseInt(nd.id, 10) === message.switch
+    )
+    Switch.status = Switch.status === STASTION_STATUS.GREEN ?
+        STASTION_STATUS.RED :
+        STASTION_STATUS.GREEN
+    updateSwitch(Switch)
+}
+
+const updateStation = (node) => {
+    var indexOfnode = network.nodes.findIndex(i => i.id === node.id)
+    network.nodes[indexOfnode].status = node.status
+    mapSvgContainer.select("rect[id='" + node.id + "']")
+        .attr('class', 'middle ' + node.type + '-node ' + node.status)
+}
+
+const updateSwitch = (Switch) => {
+    var indexOfSwitch = network.nodes.findIndex(i => i.id === Switch.id)
+    network.nodes[indexOfSwitch].status = Switch.status
+    mapSvgContainer.select("circle[id='" + Switch.id + "']")
+        .attr('class', 'middle ' + Switch.type + '-node ' + Switch.status)
+}
+
+const updateLine = (line) => {
+    var indexOfline = network.links.findIndex(i => i.source.nodeID === line.source.nodeID && i.target.nodeID === line.target.nodeID)
+    network.links[indexOfline].status = line.status
+    mapSvgContainer.select("line[source='" + line.source.nodeID + "'][target='" + line.target.nodeID + "']")
+        .attr('class', 'connect ' + line.status + '-dimmable')
+}
+
+const appendLabel = (label) => {
+    mapSvgContainer.append('text')
+        .attr('x', label.x)
+        .attr('y', label.y)
+        .attr('text-anchor', 'middle')
+        .attr('class', 'train-label')
+        .attr('id', 'lable-' + label.id)
+        .text('Train ' +  label.id)
+}
+
+const updateLabel = (label) => {
+    mapSvgContainer.select("text[id='lable-" + label.id + "']")
+        .attr('x', label.x)
+        .attr('y', label.y)
 }
 
 /* Render Function When there are Network Data
  *************************************************************/
-const render = (network) => {
+const render = (map) => {
+
+    network = map
 
     if (!mapSvgContainer)
         mapSvgContainer = createMap(PARENT_TAG_ID)
