a86dd3bc77266df0b01fa78dbbe8164780f4be57
alabbas-ali <weeamha@gmail.com>
2021-10-27 13:25:18 +0200

    :sparkles: restructure and fix home page load error

--- src/main/java/his/railway/rts/config/WebSocketConfig.java
+++ src/main/java/his/railway/rts/config/WebSocketConfig.java
@@ -2,18 +2,13 @@ package his.railway.rts.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-    
-	@Override
-    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-        stompEndpointRegistry.addEndpoint("/ws").withSockJS();
-    }
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry)
@@ -21,5 +16,9 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/railway");
         registry.setApplicationDestinationPrefixes("/app");
     }
-    
+
+	@Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/ws").withSockJS();
+    }
 }
